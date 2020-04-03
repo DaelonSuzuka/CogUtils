a892faad6b0685c357a0735de1042a872019fbe1
DaelonSuzuka <daelonsuzuka@gmail.com>
2020-04-03 00:54:22 -0400

    Setup repo

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='CogUtils',
+    version='0.1dev',
+    author='David Kincaid',
+    author_email='dlkincaid0@gmail.com',
+    packages=['cogutils',],
+    license='MIT',
+    long_description=open('README.md').read(),
+)
